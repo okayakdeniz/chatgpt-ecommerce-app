c9d91457dfce296b03da7b399d3761640823be32
Okay Akdeniz <okayakdeniz@gmail.com>
2025-11-15 18:42:21 +0300

    main.py değişti. route ile

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from .oauth import register_routes
+from app.oauth import register_routes as register_oauth_routes
 
 def register_api_routes(app):
-    register_routes(app)
+    register_oauth_routes(app)
